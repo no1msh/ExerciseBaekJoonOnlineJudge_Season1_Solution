745db1b808342e6f093aba08ad2a2190692a862d
TFQ_Moon <no1msh1217@gmail.com>
2022-04-01 23:50:17 +0900

    Day52 chapter12_sort

---
+++ ExerciseBaekJoonOnlineJudge/src/chapter12_sort/No_2750.java
@@ -0,0 +1,36 @@
+package chapter12_sort;
+
+import java.io.*;
+
+public class No_2750 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		for (int i = 0; i < N - 1; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (arr[i] > arr[j]) {
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+
+		for (int val : arr) {
+			sb.append(val).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
