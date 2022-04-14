058592bfc873732cd6cd23f5482755766b4aa5af
TFQ_Moon <no1msh1217@gmail.com>
2022-04-14 23:42:21 +0900

    Day65 chapter13_backtracking

--- ExerciseBaekJoonOnlineJudge/src/chapter13_backtracking/No_15652.java
+++ ExerciseBaekJoonOnlineJudge/src/chapter13_backtracking/No_15652.java
@@ -5,40 +5,41 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
  
-public class No_15651 {
-	
+public class No_15652 {
+ 
+	public static int N, M;
 	public static int[] arr;
-	public static int N, M; 
 	public static StringBuilder sb = new StringBuilder();
  
 	public static void main(String[] args) throws IOException {
  
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
  
-		StringTokenizer st = new StringTokenizer(br.readLine());
- 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
- 
 		arr = new int[M];
-		dfs(0);
+ 
+		dfs(1, 0);
 		System.out.println(sb);
+ 
 	}
  
-	public static void dfs(int depth) {
+	public static void dfs(int at, int depth) {
  
 		if (depth == M) {
-			for (int i = 0; i < M; i++) {
-				sb.append(arr[i]).append(' ');
+			for (int val : arr) {
+				sb.append(val).append(' ');
 			}
 			sb.append('\n');
 			return;
 		}
  
-		for (int i = 1; i <= N; i++) {
+		for (int i = at; i <= N; i++) {
 			arr[depth] = i;
-			dfs(depth + 1);
+			dfs(i, depth + 1);
 		}
+ 
 	}
  
 }
