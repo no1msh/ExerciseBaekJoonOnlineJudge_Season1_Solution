fca1875fc258d603ecfa7b08ac316ca1b275c96b
TFQ_Moon <no1msh1217@gmail.com>
2022-04-29 23:26:13 +0900

    Day80 chapter14_number_theory

---
+++ ExerciseBaekJoonOnlineJudge/src/chapter14_number_theory/No_1676.java
@@ -0,0 +1,22 @@
+package chapter14_number_theory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class No_1676 {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int num = Integer.parseInt(br.readLine());
+		int count = 0;
+ 
+		while (num >= 5) {
+			count += num / 5;
+			num /= 5;
+		}
+		System.out.println(count);
+	}
+ 
+}
