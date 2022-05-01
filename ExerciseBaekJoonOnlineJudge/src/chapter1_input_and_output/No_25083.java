5bfa227ff1a40569cd677e945d13a08a1f8c0106
TFQ_Moon <no1msh1217@gmail.com>
2022-05-01 23:01:02 +0900

    Day82 chapter1_input_and_output

---
+++ ExerciseBaekJoonOnlineJudge/src/chapter1_input_and_output/No_25083.java
@@ -0,0 +1,14 @@
+package chapter1_input_and_output;
+
+public class No_25083 {
+
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+
+}
